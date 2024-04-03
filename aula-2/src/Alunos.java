af857736d9b1c3d426d7bdf682ed80ab5fdf2945
Lucas Teles <lucasouzateles@gmail.com>
2024-04-03 20:56:09 -0300

    adicionando aula 02

--- aula-2/src/Alunos.java
+++ aula-2/src/Alunos.java
@@ -1,10 +1,44 @@
+import java.util.Scanner;
+
 public class Alunos {
     String nome;
     String raca;
     int idade;
 
     //método construtor
-    public Alunos(){
+    public Alunos(String nome, String raca, int idade){
+
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+    }
+
+    public void imprimirDadosDoAluno() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Raça: " + idade);
+        System.out.println("Idade: " + raca);
+
+    }
+
+    public static void main(String[] args){
+        Scanner inputDoTeclado = new Scanner(System.in);
+
+        System.out.println("Digite o nome do Aluno: ");
+        String nome = inputDoTeclado.nextLine();
+
+        System.out.println("Digite a raça do aluno: ");
+        String raca = inputDoTeclado.nextLine();
+
+        System.out.println("Digite a idade do aluno: ");
+        int idade = inputDoTeclado.nextInt();
+        inputDoTeclado.nextLine();
+
+        Alunos aluno = new Alunos(nome, raca, idade);
+
+        System.out.println("----------------- DADOS DO ALUNO ----------------");
+        aluno.imprimirDadosDoAluno();
+
+        inputDoTeclado.close();
 
     }
 
