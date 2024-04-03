8e8833206ee7858f553abeea94d3d8f2605d9882
Lucas Teles <lucasouzateles@gmail.com>
2024-04-03 19:16:10 -0300

    organizando o código

--- aula-1/print-simples-funcionario/src/application/Program.java
+++ aula-1/print-simples-funcionario/src/application/Program.java
@@ -1,11 +1,10 @@
 package application;
 
 public class Program {
+    public static String nomeFuncionario = "Rakin";
+    public static String cargo = "Analista de sistemas";
+    public static double salario = 4166.00;
     public static void main(String[] args){
-        String nomeFuncionario = "Rakin";
-        String cargo = "Analista de sistemas";
-        double salario = 4166.00;
-
         System.out.println("Funcionario: " + nomeFuncionario);
         System.out.println("Cargo: " + cargo);
         System.out.println("Salário: " + salario);
