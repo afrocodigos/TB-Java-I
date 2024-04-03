91e9f4ad2e5c6c774973801f194cff00a094c5ad
Lucas Teles <lucasouzateles@gmail.com>
2024-04-03 16:04:35 -0300

    Editando o readme e respondendo o primeiro desafio

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TB-Java-I
 Turma Backend Java I
+
+## Desafios
+
+*** Na pasta aula I foi respondido o primeiro desafio proposto
---
+++ aula-1/print-simples-funcionario/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Program {
+    public static void main(String[] args){
+        String nomeFuncionario = "Rakin";
+        String cargo = "Analista de sistemas";
+        double salario = 4166.00;
+
+        System.out.println("Funcionario: " + nomeFuncionario);
+        System.out.println("Cargo: " + cargo);
+        System.out.println("Salário: " + salario);
+
+    }
+}
