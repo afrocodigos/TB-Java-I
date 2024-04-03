71bc3d72f207db44e84908a0ffed116ecdffcd68
Lucas Teles <lucasouzateles@gmail.com>
2024-04-03 19:22:00 -0300

    adicionando gitignore

--- aula-1/print-simples-funcionario/src/application/Program.java
+++ aula-1/print-simples-funcionario/src/application/Program.java
@@ -1,9 +1,9 @@
 package application;
 
 public class Program {
-    public static String nomeFuncionario = "Rakin";
-    public static String cargo = "Analista de sistemas";
-    public static double salario = 4166.00;
+    static String nomeFuncionario = "Rakin";
+    static String cargo = "Analista de sistemas";
+    static double salario = 4166.00;
     public static void main(String[] args){
         System.out.println("Funcionario: " + nomeFuncionario);
         System.out.println("Cargo: " + cargo);
