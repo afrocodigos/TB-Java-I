1d36875543ae249583126f5ac9dcc68dd839ff5e
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-04-10 11:05:59 -0300

    add java I
    add conteudo e exemplos

---
+++ exemplos/desafioSemana/printFuncionario/printBasico.java
@@ -0,0 +1,12 @@
+public class printBasico {
+
+    public static String nomeFuncionario = "José";
+    public static String cargoFuncionario = "Carteiro";
+    public static double salarioFuncionario = 5268.00;
+
+    public static void main(String[] args) {
+        System.out.println("Nome do funcionário: " + nomeFuncionario);
+        System.out.println("Cargo do funcionário: " + cargoFuncionario);
+        System.out.println("Salário do funcionário: R$ " + salarioFuncionario);
+    }
+}
---
+++ exemplos/desafioSemana/printFuncionario/printClasse.java
@@ -0,0 +1,8 @@
+import funcionarios.Funcionario;
+
+public class printClasse {
+    public static void main(String[] args){
+        Funcionario funcionario1 = new Funcionario("Maria","Professora",2886.24);
+        funcionario1.imprimeFuncionario();
+    }
+}
