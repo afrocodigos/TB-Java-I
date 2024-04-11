1cf730100b5e04c96b7878affe44a7a2563e25a3
julio-cesar96 <julio_cbs96@hotmail.com>
2024-04-11 09:39:46 -0300

    solucao da classe triangulo

---
+++ src/triangulo/Triangulo.java
@@ -0,0 +1,24 @@
+package triangulo;
+
+public class Triangulo {
+    private double lado1;
+    private double lado2;
+    private double lado3;
+    
+    public Triangulo(double lado1, double lado2, double lado3) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.lado3 = lado3;
+    }
+    
+    // Método para verificar o tipo de triângulo
+    public String tipoTriangulo() {
+        if (lado1 == lado2 && lado2 == lado3) {
+            return "Equilátero";
+        } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+            return "Isósceles";
+        } else {
+            return "Escaleno";
+        }
+    }
+}
