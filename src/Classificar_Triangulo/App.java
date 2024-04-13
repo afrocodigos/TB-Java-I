9a7dfeb9894a5667e7d7581bb2d261388de0e436
heloisebastos <heloisebastos28@hotmail.com>
2024-04-12 21:32:58 -0400

    :bug: fix: troca tipo do atributo Double para double

--- src/Classificar_Triangulo/App.java
+++ src/Classificar_Triangulo/App.java
@@ -1,11 +1,25 @@
 package Classificar_Triangulo;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        String tipo;
-        Triangulo triangulo = new Triangulo(3.0, 3.0, 3.0);
-        tipo = triangulo.classificarTriangulo();
+        Scanner read = new Scanner(System.in);
+        System.out.println("Digite um valor pro lado 1 :");
+        double lado1 = read.nextDouble();
+        read.nextLine();
+        System.out.println("Digite um valor pro lado 2 :");
+        double lado2 = read.nextDouble();
+        read.nextLine();
+        System.out.println("Digite um valor pro lado 3 :");
+        double lado3 = read.nextDouble();
+        read.nextLine();
+
+        Triangulo objeto = new Triangulo(lado1, lado2, lado3);
+        String tipo = objeto.classificarTriangulo();
         System.out.println("O tipo do triângulo é: " + tipo);
+
+        read.close();
     }
 
 }
--- src/Classificar_Triangulo/Triangulo.java
+++ src/Classificar_Triangulo/Triangulo.java
@@ -2,37 +2,41 @@ package Classificar_Triangulo;
 
 public class Triangulo {
 
-    private Double lado1;
-    private Double lado2;
-    private Double lado3;
+    private double lado1;
+    private double lado2;
+    private double lado3;
 
-    public Triangulo(Double lado1, Double lado2, Double lado3) {
+    public Triangulo() {
+
+    }
+
+    public Triangulo(double lado1, double lado2, double lado3) {
         this.lado1 = lado1;
         this.lado2 = lado2;
         this.lado3 = lado3;
     }
 
-    public Double getLado1() {
+    public double getLado1() {
         return lado1;
     }
 
-    public void setLado1(Double lado1) {
+    public void setLado1(double lado1) {
         this.lado1 = lado1;
     }
 
-    public Double getLado2() {
+    public double getLado2() {
         return lado2;
     }
 
-    public void setLado2(Double lado2) {
+    public void setLado2(double lado2) {
         this.lado2 = lado2;
     }
 
-    public Double getLado3() {
+    public double getLado3() {
         return lado3;
     }
 
-    public void setLado3(Double lado3) {
+    public void setLado3(double lado3) {
         this.lado3 = lado3;
     }
 
