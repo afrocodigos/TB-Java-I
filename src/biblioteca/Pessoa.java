3cf3a383b56e2f516a1682928ee33707ae9a41c8
Anny <anny_s_araujo@hotmail.com>
2024-04-22 22:51:35 -0300

    Desafio da aula do dia 11 de abril

---
+++ src/biblioteca/Pessoa.java
@@ -0,0 +1,28 @@
+package biblioteca;
+
+public class Pessoa {
+    private String nome;
+    private int id;
+
+    public Pessoa(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
